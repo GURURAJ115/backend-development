c4012f6af757ee0a03277e34d91670329e81a65b
GURURAJ115 <gururajsnaik115@gmail.com>
2025-01-01 18:05:22 +0530

    Global catches in input validation

--- input_validation.js
+++ input_validation.js
@@ -5,8 +5,14 @@ app.use(express.json());
 app.post("/health-checkup",function(req,res){
     const kidneys = req.body.kidneys;
     const kidneyLength = kidneys.length;
-
     res.send("you have " + kidneyLength+" kidneys");
+})
+
+//global catches - should be added only once at the end
+app.use(function(err,req,res,next){
+    res.json({
+        msg: "Sorry something is up with our server"
+    })
 })
 
 app.listen(3000);
